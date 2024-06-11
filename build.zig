6a0b404ea957547eeaa572b5f69f90224925a1f8
csirak1528 <calebsirak@gmail.com>
2024-06-11 06:43:12 +0200

    kernel: hello world

---
+++ build.zig
@@ -0,0 +1,79 @@
+const std = @import("std");
+const Builder = @import("std").build.Builder;
+const Target = @import("std").Target;
+const CrossTarget = @import("std").zig.CrossTarget;
+const Feature = @import("std").Target.Cpu.Feature;
+
+const objFiles = [_][]u8{"main"};
+
+pub fn build(b: *Builder) void {
+    const target = CrossTarget{ .cpu_arch = Target.Cpu.Arch.riscv64, .os_tag = Target.Os.Tag.freestanding };
+
+    const optimize = b.standardOptimizeOption(.{});
+
+    const kernel = b.addExecutable(.{ .name = "kernel", .root_source_file = .{ .path = "kernel/entry.zig" }, .optimize = optimize, .target = target, .linkage = std.build.CompileStep.Linkage.dynamic });
+    kernel.code_model = .medium;
+    const start = b.addObject(.{
+        .name = "start",
+        .root_source_file = .{ .path = "kernel/start.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    start.code_model = .medium;
+    const main = b.addObject(.{
+        .name = "main",
+        .root_source_file = .{ .path = "kernel/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    main.code_model = .medium;
+
+    const trampoline = b.addObject(.{
+        .name = "trampoline",
+        .root_source_file = .{ .path = "kernel/trampoline.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    trampoline.code_model = .medium;
+
+    trampoline.addIncludePath(.{ .path = "kernel" });
+
+    // const lib = b.addObject(.{
+    //     .name = "lib",
+    //     .root_source_file = .{ .path = "kernel/lib.zig" },
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+
+    kernel.addIncludePath(.{ .path = "kernel" });
+    kernel.setLinkerScript(.{ .path = "kernel/kernel.ld" });
+    kernel.addObject(main);
+    kernel.addObject(start);
+    kernel.addObject(trampoline);
+    // kernel.addObject(lib);
+    b.installArtifact(kernel);
+
+    const run_cmd_str = [_][]const u8{
+        "qemu-system-riscv64",
+        "-m",
+        "512",
+        "-smp",
+        "1",
+        "-no-reboot",
+        "-nographic",
+        "-bios",
+        "none",
+        "-M",
+        "virt",
+        "-kernel",
+        "./zig-out/bin/kernel",
+        // "-S",
+        // "-s",
+    };
+
+    const run_cmd = b.addSystemCommand(&run_cmd_str);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    const run_step = b.step("run", "Run the kernel");
+    run_step.dependOn(&run_cmd.step);
+}
