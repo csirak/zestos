e7b3136794923c6ac7e5629d444a490089c8f436
csirak1528 <calebsirak@gmail.com>
2024-08-03 04:18:25 -0400

    pagetable: add dealloc functions

--- kernel/mem/pagetable.zig
+++ kernel/mem/pagetable.zig
@@ -6,6 +6,7 @@ const lib = @import("../lib.zig");
 const PAGETABLE_SIZE = riscv.PGSIZE / @sizeOf(u64);
 // 9 bit mask
 const PX_MASK = 0x1ff;
+const PTE_FLAGS_MASK = 0x3ff;
 const PG_OFFSET_SIZE = 12;
 const PT_INDEX_SIZE = 9;
 const PTE_FLAGS_SIZE = 10;
@@ -15,10 +16,19 @@ const Table = *[PAGETABLE_SIZE]u64;
 const Self = @This();
 table: Table,
 
-pub fn init() Self {
+pub fn init() !Self {
     return Self{
-        .table = makeTable() catch unreachable,
+        .table = makeTable() catch |e| return e,
     };
+}
+
+pub fn userFree(self: *Self, size: u64) !void {
+    try self.unMapPages(riscv.TRAMPOLINE, 1, false);
+    try self.unMapPages(riscv.TRAPFRAME, 1, false);
+    if (size > 0) {
+        try self.unMapPages(0, mem.pageAlignUp(size), true);
+    }
+    try freeTable(self.table);
 }
 
 pub fn getPhysAddrFromVa(self: *Self, virtual_address: u64, alloc: bool) !*u64 {
@@ -33,38 +43,63 @@ pub fn getPhysAddrFromVa(self: *Self, virtual_address: u64, alloc: bool) !*u64 {
         if (pte.* & mem.PTE_V != 0) {
             cur_pagetable = @ptrCast(pageTableEntryToPhysAddr(pte.*));
         } else if (alloc) {
-            const table = makeTable() catch |e| return e;
+            const table = try makeTable();
             pte.* = physAddrToPTE(&table[0]) | mem.PTE_V;
             cur_pagetable = table;
         } else {
-            return error.NoAllocation;
+            return error.PageTableLevelNotDeepEnough;
         }
     }
     return &cur_pagetable[pageTableLevelIndex(virtual_address, 0)];
 }
 
-pub fn setSatp(self: *Self) void {
-    riscv.w_satp(mem.MAKE_SATP(@intFromPtr(self.table)));
-}
-
 pub fn mapPages(self: *Self, virtual_address: u64, physical_address: u64, size: u64, flags: u16) !void {
-    var cur_page = mem.pageAlignDown(virtual_address);
-    var cur_physical_address = physical_address;
+    const virtual_address_page_aligned = mem.pageAlignDown(virtual_address);
+
+    var page_offset: u64 = 0;
     const last_page = mem.pageAlignDown(virtual_address + size - 1);
 
-    if ((last_page - cur_page) % riscv.PGSIZE != 0) {
+    if ((last_page - virtual_address_page_aligned) % riscv.PGSIZE != 0) {
         lib.kpanic("not page align");
     }
 
-    while (cur_page <= last_page) {
-        const pte = self.getPhysAddrFromVa(cur_page, true) catch |e| return e;
+    while (virtual_address_page_aligned + page_offset <= last_page) : (page_offset += riscv.PGSIZE) {
+        const pte = try self.getPhysAddrFromVa(virtual_address_page_aligned + page_offset, true);
         if (pte.* & mem.PTE_V != 0) {
             return error.MappedPageAlreadyAllocated;
         }
-        pte.* = physAddrToPTE(@ptrFromInt(cur_physical_address)) | flags | mem.PTE_V;
-        cur_page += riscv.PGSIZE;
-        cur_physical_address += riscv.PGSIZE;
+        pte.* = physAddrToPTE(@ptrFromInt(physical_address + page_offset)) | flags | mem.PTE_V;
     }
+}
+
+pub fn unMapPages(self: *Self, virtual_address: u64, num_pages: u64, freePages: bool) !void {
+    if (virtual_address % riscv.PGSIZE != 0) {
+        return error.NotPageAligned;
+    }
+
+    var cur_virtual_address = virtual_address;
+
+    while (cur_virtual_address <= virtual_address + num_pages * riscv.PGSIZE) : (cur_virtual_address += riscv.PGSIZE) {
+        const pte = try self.getPhysAddrFromVa(cur_virtual_address, false);
+        if (pte.* & mem.PTE_V == 0) {
+            return error.PageNotMapped;
+        }
+        if (pageTableEntryFlags(pte.*) == mem.PTE_V) {
+            return error.NotPageTableLeaf;
+        }
+        if (freePages) {
+            KMem.free(@intFromPtr(pageTableEntryToPhysAddr(pte.*)));
+        }
+        pte.* = 0;
+    }
+}
+
+pub inline fn setSatp(self: *Self) void {
+    riscv.w_satp(self.getAsSatp());
+}
+
+pub inline fn getAsSatp(self: *Self) u64 {
+    return mem.MAKE_SATP(@intFromPtr(self.table));
 }
 
 inline fn pageTableLevelIndex(address: u64, level: u6) u64 {
@@ -76,13 +111,33 @@ inline fn pageTableEntryToPhysAddr(pte: u64) *u64 {
     return @ptrFromInt((pte >> PTE_FLAGS_SIZE) << PG_OFFSET_SIZE);
 }
 
+inline fn pageTableEntryFlags(pte: u64) u64 {
+    return pte & PTE_FLAGS_MASK;
+}
+
 inline fn physAddrToPTE(address: *u64) u64 {
     const pa = @intFromPtr(address);
     return (pa >> PG_OFFSET_SIZE) << PTE_FLAGS_SIZE;
 }
+
 fn makeTable() !Table {
-    const page = KMem.alloc() catch |e| return e;
-    const table: Table = @ptrCast(page);
+    const table: Table = @ptrCast(try KMem.alloc());
     @memset(table, 0);
     return table;
+}
+
+fn freeTable(table: Table) !void {
+    for (0..PAGETABLE_SIZE) |i| {
+        const pte = table[i];
+        // is parent node?
+        if ((pte & mem.PTE_V) != 0 and (pte & (mem.PTE_R | mem.PTE_W | mem.PTE_X) == 0)) {
+            const child = pageTableEntryToPhysAddr(pte);
+            try freeTable(@ptrCast(child));
+            table[i] = 0;
+        } else if (pte & mem.PTE_V != 0) {
+            return error.NotFreedLeafNodesExist;
+        }
+    }
+
+    KMem.free(@intFromPtr(table));
 }
