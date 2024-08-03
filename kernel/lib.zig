ef7c997bfb7ca21890b83234e23f553de06d966a
csirak1528 <calebsirak@gmail.com>
2024-08-03 04:12:15 -0400

    lib: refactor

--- kernel/lib.zig
+++ kernel/lib.zig
@@ -1,35 +1,40 @@
 const std = @import("std");
+const riscv = @import("riscv.zig");
 
 comptime {
     asm (
-        \\.globl putChar
-        \\putChar:
+        \\.globl put_char
+        \\put_char:
         \\.equ     UART_REG_TXFIFO, 0
         \\.equ     UART_BASE, 0x10000000
         \\li       t0, UART_BASE           # load UART base address
         \\
-        \\.Lputchar_loop:
+        \\.Lput_char_loop:
         \\lw       t1, UART_REG_TXFIFO(t0) # read UART TX FIFO status
         \\li       t2, 0x80000000
         \\and      t1, t1, t2
-        \\bnez     t1, .Lputchar_loop      # if TX FIFO is full, wait
+        \\bnez     t1, .Lput_char_loop      # if TX FIFO is full, wait
         \\
         \\sw       a0, UART_REG_TXFIFO(t0) # write character to TX FIFO
         \\ret
     );
 }
 
-extern fn putChar(c: u8) void;
+extern fn put_char(c: u8) void;
 
 pub fn print(s: []const u8) void {
     for (s) |c| {
-        putChar(c);
+        put_char(c);
     }
 }
 
 pub fn println(s: []const u8) void {
     print(s);
-    putChar('\n');
+    put_char('\n');
+}
+
+pub fn printErr(e: anyerror) void {
+    println(@errorName(e));
 }
 
 pub fn printInt(n: u64) void {
@@ -49,4 +54,17 @@ pub fn kpanic(msg: []const u8) noreturn {
     print("kernel panic: ");
     println(msg);
     while (true) {}
+}
+
+pub fn strCopy(dst: []u8, src: []const u8, size: u64) void {
+    for (0..size) |i| {
+        dst[i] = src[i];
+    }
+}
+
+pub fn coreLog(comptime s: []const u8) void {
+    const id = riscv.cpuid();
+    const idchar = [_]u8{ @intCast(id + 48), ' ' };
+    const out = "zest core: " ++ idchar ++ s;
+    println(out);
 }
